0e0711944777d4616b7a945a4cc21b215ac8c1a0
Juan Sebastian <juanumpele29@gmail.com>
2023-06-05 14:10:23 +0700

    create seeder: auth_user

---
+++ seeders/20230531022415-auth_user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+     await queryInterface.bulkInsert('auth_users', [{
+      nama_pengguna : "joestin",
+      email : "juanumpele29@gmail.com",
+      kata_sandi : "$2b$10$B73vva29VVqUmh0M8WbmVOQwOVJu3LNmq2TLi55MXh1AwLugY1Yqe",
+      nomor_telepon : "082313235678",
+      status : "active",
+      role : "admin",
+      id_peternakan : "1"
+      
+
+     }], {});
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.bulkDelete('auth_users', null, {});
+  }
+};
