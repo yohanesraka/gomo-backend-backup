bedcb11a0367e669281bc25735ac72a3e05a34ec
Juan Sebastian <juanumpele29@gmail.com>
2023-06-07 22:04:00 +0700

    update produksi controller

--- controllers/produksi_susu.controller.js
+++ controllers/produksi_susu.controller.js
@@ -10,12 +10,16 @@ const produksiController = (db) =>{
     const ProduksiController = Router();
     
 
-    //Get List Produksi Susu
     ProduksiController.get('/', authentication,adminMiddleware, async(req, res, next)=>{
         const list = await s$produksi.getDataProduksi(req);
         response.sendResponse(res, list);
     });
 
+    ProduksiController.get('/:id_ternak', authentication, adminMiddleware, async (req, res, next) => {
+        const listByID = await s$produksi.getDataProduksiByIdTernak(req);
+        response.sendResponse(res, listByID);
+    });
+    
     /** 
      * @param {number} id_peternakan
      * @param {number} id_ternak
@@ -25,25 +29,23 @@ const produksiController = (db) =>{
      * @param {string} tanggal_produksi
     */
 
-    //Create New Produksi Susu
     ProduksiController.post('/', authentication, adminMiddleware, async (req, res, next)=>{
         const add = await s$produksi.createDataProduksi(req);
         response.sendResponse(res, add);
         // console.log(res);
     });
 
-    ProduksiController.put('/', authentication, adminMiddleware, async (req, res, next) =>{
+    ProduksiController.put('/', authentication, adminMiddleware, async (req, res, next) =>{   
         const edit = await s$produksi.updateDataProduksi(req);
-        response.sendResponse(req, edit);
+        response.sendResponse(res, edit);
     });
 
-    ProduksiController.delete('/', authentication, adminMiddleware, async (req, res, next) =>{
-        const del = await s$produksi.delelteDataProduksi(req);
-        response.sendResponse(req, del);
+    ProduksiController.delete('/:id_produksi_susu', authentication, adminMiddleware, async (req, res, next) => {
+        req.body.id_produksi_susu = req.params.id_produksi_susu;
+        const deleteResult = await s$produksi.deleteDataProduksi(req);
+        response.sendResponse(res, deleteResult);
     });
-
-    
-
+      
     return ProduksiController;
      
 }
