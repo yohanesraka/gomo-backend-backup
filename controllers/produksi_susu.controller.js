0b12125522428236421335de5e60d22b53431b87
nantalira <kampoengbola6@gmail.com>
2023-06-13 13:54:32 +0700

    add: get total data produksi API

--- controllers/produksi_susu.controller.js
+++ controllers/produksi_susu.controller.js
@@ -1,57 +1,55 @@
-const {Router} = require('express');
-const produksiService = require('../services/produksi_susu.service');
-const response = require('../utils/response');
-const authentication = require('../middlewares/authentication');
-const { adminMiddleware } = require('../middlewares/authorization');
-const { string } = require('joi');
+const { Router } = require("express");
+const produksiService = require("../services/produksi_susu.service");
+const response = require("../utils/response");
+const authentication = require("../middlewares/authentication");
+const { adminMiddleware } = require("../middlewares/authorization");
+const { string } = require("joi");
 
-const produksiController = (db) =>{
+const produksiController = (db) => {
     const s$produksi = produksiService(db);
     const ProduksiController = Router();
-    
 
-    ProduksiController.get('/', authentication,adminMiddleware, async(req, res, next)=>{
+    ProduksiController.get("/", authentication, adminMiddleware, async (req, res, next) => {
         const list = await s$produksi.getDataProduksi(req);
         response.sendResponse(res, list);
     });
 
-    ProduksiController.get('/:id_ternak', authentication, adminMiddleware, async (req, res, next) => {
+    ProduksiController.get("/total", authentication, adminMiddleware, async (req, res, next) => {
+        const listTotal = await s$produksi.getTotalDataProduksi(req);
+        response.sendResponse(res, listTotal);
+    });
+
+    ProduksiController.get("/:id_ternak", authentication, adminMiddleware, async (req, res, next) => {
         const listByID = await s$produksi.getDataProduksiByIdTernak(req);
         response.sendResponse(res, listByID);
     });
 
-    ProduksiController.get('/total', authentication, adminMiddleware, async (req, res, next) =>{
-        const listTotal = await s$produksi.getTotalDataProduksi(req);
-        response.sendResponse(res, listTotal);
-    });
-    
-    /** 
+    /**
      * @param {number} id_peternakan
      * @param {number} id_ternak
      * @param {string} produksi_pagi
      * @param {string} produksi_sore
      * @param {string} kualitas
      * @param {string} tanggal_produksi
-    */
+     */
 
-    ProduksiController.post('/', authentication, adminMiddleware, async (req, res, next)=>{
+    ProduksiController.post("/", authentication, adminMiddleware, async (req, res, next) => {
         const add = await s$produksi.createDataProduksi(req);
         response.sendResponse(res, add);
         // console.log(res);
     });
 
-    ProduksiController.put('/', authentication, adminMiddleware, async (req, res, next) =>{   
+    ProduksiController.put("/", authentication, adminMiddleware, async (req, res, next) => {
         const edit = await s$produksi.updateDataProduksi(req);
         response.sendResponse(res, edit);
     });
 
-    ProduksiController.delete('/:id_produksi_susu', authentication, adminMiddleware, async (req, res, next) => {
+    ProduksiController.delete("/:id_produksi_susu", authentication, adminMiddleware, async (req, res, next) => {
         req.body.id_produksi_susu = req.params.id_produksi_susu;
         const deleteResult = await s$produksi.deleteDataProduksi(req);
         response.sendResponse(res, deleteResult);
     });
-      
+
     return ProduksiController;
-     
-}
+};
 module.exports = produksiController;
